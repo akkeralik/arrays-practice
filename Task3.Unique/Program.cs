0e2c7cb95275a9a2e6d3248ae9d1a4bc9fc54c75
Shepeleva <alisashepeleva2008@gmail.com>
2026-09-17 23:01:27 +0300

    Task3: уникальные элементы без LINQ

--- Task3.Unique/Program.cs
+++ Task3.Unique/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static int[] GetUnique(int[] source)
+    {
+        int[] temp = new int[source.Length];
+        int count = 0;
+
+        for (int i=0; i<source.Length; i++)
+        {
+            int found = 0;
+
+            for (int j = 0; j < count; j++)
+            {
+                if (source[i] == temp[j])
+                {
+                    found = 1;
+                }
+            }
+            if (found == 0)
+            {
+                temp[count] = source[i];
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        for (int i = 0; i<count; i++)
+        {
+            result[i] = temp[i];
+        }
+        return result;
+    }
+    static void Main()
+    {
+        int[] numbers = {1, 2, 2, 3, 4, 4, 5};
+        Console.WriteLine("Исходный: "+string.Join(", ", numbers));
+        int[] unique = GetUnique(numbers);
+        Console.WriteLine("Уникальные: "+string.Join(", ", unique));
+    }
+}
