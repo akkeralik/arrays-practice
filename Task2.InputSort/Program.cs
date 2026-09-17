cdf45c8d0c0a4f4a2ce8e863e70eeaa27f55c902
543 компьютерный класс <stud543@ogu.local>
2026-09-17 15:25:12 +0300

    Task2: ввод, реверс и сортировка массива

--- Task2.InputSort/Program.cs
+++ Task2.InputSort/Program.cs
@@ -1,1 +1,56 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System; 
+int n = 0;
+while (n<=0)
+{
+    try 
+    {
+      Console.Write("Введите количество элементов: ");
+      n = int.Parse(Console.ReadLine());  
+    }
+    catch (FormatException) //буквы вместо числа
+    {
+        Console.WriteLine("Ошибка ввода.");
+    }
+    catch(OverflowException) //слишком огромное число
+    {
+        Console.WriteLine("Ошибка ввода.");
+    }
+    
+}
+int[] numbers = new int[n];
+for (int i = 0; i<numbers.Length; i++)
+{
+    try
+    {
+        Console.Write("Элемент [" + i + "]: ");
+        numbers[i] = int.Parse(Console.ReadLine());  
+    }
+    catch (FormatException)
+    {
+        
+        Console.WriteLine("Ошибка ввода.");
+        i--; //при ошибке заставляет не переходить к некст элементу а еще раз запрашивает элемент
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Ошибка ввода.");
+        i--; 
+    }
+}
+Console.WriteLine("Исходный массив: "+string.Join(", ", numbers));
+Console.Write("Обратный порядок: ");
+for (int i = numbers.Length - 1; i>=0; i--)
+{
+    Console.Write(numbers[i]);
+    if (i>0)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine();
+Array.Sort(numbers); //переставляет числа от меньш к большему 
+Console.WriteLine("По возрастанию: "+string.Join(", ", numbers));
+Array.Reverse(numbers); //переставляет числа от большего к меньшему
+Console.WriteLine("По убыванию: "+string.Join(", ", numbers));
+
+
