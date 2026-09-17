da1d674134a74c9fbe16a99627856b4bfe579b91
543 компьютерный класс <stud543@ogu.local>
2026-09-17 14:54:22 +0300

    Task1: add program code

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+Random random = new Random();
+int[] numbers = new int[10];
+int sum = 0; 
+double product = 1;
+int evenCount = 0;
+for (int i =0; i<numbers.Length; i++)
+{
+    numbers[i] = random.Next(1, 101);
+    sum = sum + numbers[i];
+    product = product * numbers[i];
+
+    if (numbers[i]%2==0)
+    {
+        evenCount++
+    }
+}
+double average = (double)sum/numbers.Length;
+int aboveAverageCount = 0;
+for (int i = 0; i<numbers.Length; i++)
+{
+    if (numbers[i]>average)
+    {
+        aboveAverageCount++;
+    }
+}
+
+Console.WriteLine("Массив: " + string.Join(", ", numbers));
+Console.WriteLine("Сумма: "+sum);
+Console.WriteLine("Произведение: "+product);
+Console.WriteLine("Четных чисел: "+evenCount);
+Console.WriteLine("Больше среднего "+average+"): "+abiveAverageCount);
